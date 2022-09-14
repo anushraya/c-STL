97e163a14e26bee08c65f627b8ecd073ba57f56c
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-09-14 22:13:27 +0530

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,21 @@
+//for sorting first three elements
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[4]= {5,4,1,2};
+    sort(a,a+3);
+    for(int i=0;i<4;i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+//for sorting all 4 elements
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[4]={5,2,3,7};
+    sort(a,a+4);
+    for (int i=0;i<4;i++){
+        cout<<a[i]<<" ";
+    }
+}
